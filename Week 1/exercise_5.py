aa0f8bacaacc37e85338e872995d6c05ba2b4d59
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-03-17 12:31:32 -0300

    Update exercise_5.py

---
+++ Week 1/exercise_5.py
@@ -0,0 +1,38 @@
+def f(x):
+  return x;
+
+def equal(xk1, xk, a, b, k):
+  m = abs(xk1 - xk)
+  e = (b - a) / (2**(k+1))
+
+  if m < e:
+      return True
+  return False
+
+def bisection_method(a, b, ant, k):
+  x = (a + b) / 2.0
+
+  if f(x) == 0.0 or (k != 0 and equal(x, ant, a, b, k)):
+    print("The root is ", x)
+  elif f(a) * f(x) <= 0:
+    bisection_method(a, x, x, k+1)
+  else:
+    bisection_method(x, b, x, k+1)
+
+def main():
+  print("Type the interval [a, b] to see if there is a way to apply bisection method.")
+  print("Type a:")
+  a = float(input())
+  print("Type b:")
+  b = float(input())
+
+  if f(a) * f(b) < 0:
+    print("Bisection method can be applied!")
+    bisection_method(a, b, 0, 0)
+    #print(a, b)
+  else:
+    print("Sorry! Bisection method can not be used in this interval!")
+
+
+if __name__ == "__main__":
+    main()
