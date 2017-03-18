8fe0e7ebecddb65c2069054e37e7a41f750b050a
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@gmail.com>
2017-03-18 08:16:45 -0300

    now you can choose which letter of the exercise, can put many tests once and only show the necessary digits

--- Week 1/exercise_2.py
+++ Week 1/exercise_2.py
@@ -1,9 +1,11 @@
 from math import sin, log1p
 
-def f(x):
-  return x**5 + x - 1
-  # return sin(x) - 6*x - 5
-  # return loglp(x) + x**2 - 3
+def f(x, op):
+  if op == 'A':
+    return x**5 + x - 1
+  if op == 'B':
+    return sin(x) - 6*x - 5
+  return loglp(x) + x**2 - 3
 
 def equal(xk1, xk):
   m = abs(xk1 - xk)
@@ -13,30 +15,37 @@ def equal(xk1, xk):
       return True
   return False
 
-def bisection_method(a, b, ant, k):
+def bisection_method(a, b, ant, op, k):
   x = (a + b) / 2.0
 
-  if f(x) == 0.0 or (k != 0 and equal(x, ant)):
-    print("The root is ", x)
-  elif f(a) * f(x) <= 0:
-    bisection_method(a, x, x, k+1)
+  if f(x, op) == 0.0 or (k != 0 and equal(x, ant)):
+    print("We needed just {} iterations".format(k))
+    print("The root is {:.8f}".format(x))
+  elif f(a, op) * f(x, op) <= 0:
+    bisection_method(a, x, x, op, k+1)
   else:
-    bisection_method(x, b, x, k+1)
+    bisection_method(x, b, x, op, k+1)
+
 
 def main():
-  print("Type the interval [a, b] to see if there is a way to apply bisection method.")
-  print("Type a:")
-  a = float(input())
-  print("Type b:")
-  b = float(input())
+  print("Choose the function you want to test! You can choose between A, B and C.")
+  op = input()
 
-  if f(a) * f(b) < 0:
-    print("Bisection method can be applied!")
-    bisection_method(a, b, 0, 0)
-    #print(a, b)
-  else:
-    print("Sorry! Bisection method can not be used in this interval!")
+  print("How many intervals you want to try?")
+  n = int(input())
 
+  for i in range(n):
+    print("Type the interval [a, b] to see if there is a way to apply bisection method.")
+    print("Type a:")
+    a = float(input())
+    print("Type b:")
+    b = float(input())
+
+    if f(a, op) * f(b, op) < 0:
+      print("Bisection method can be applied!")
+      bisection_method(a, b, 0, op, 0)
+    else:
+      print("Sorry! Bisection method can not be used in this interval!")
 
 if __name__ == "__main__":
     main()
--- Week 1/exercise_4.py
+++ Week 1/exercise_4.py
@@ -1,7 +1,9 @@
-def f(x):
-  return x**2 - 2
-  #return x**2 - 3
-  #return x**2 - 5
+def f(x, op):
+  if op == 'A':
+    return x**2 - 2
+  if op == 'B':
+    return x**2 - 3
+  return x**2 - 5
 
 def equal(xk1, xk):
   m = abs(xk1 - xk)
@@ -11,30 +13,36 @@ def equal(xk1, xk):
       return True
   return False
 
-def bisection_method(a, b, ant, k):
+def bisection_method(a, b, ant, op, k):
   x = (a + b) / 2.0
 
-  if f(x) == 0.0 or (k != 0 and equal(x, ant)):
-    print("The root is ", x)
-  elif f(a) * f(x) <= 0:
-    bisection_method(a, x, x, k+1)
+  if f(x, op) == 0.0 or (k != 0 and equal(x, ant)):
+    print("We needed just {} iterations".format(k))
+    print("The root is {:.8f}".format(x))
+  elif f(a, op) * f(x, op) <= 0:
+    bisection_method(a, x, x, op, k+1)
   else:
-    bisection_method(x, b, x, k+1)
+    bisection_method(x, b, x, op, k+1)
 
 def main():
-  print("Type the interval [a, b] to see if there is a way to apply bisection method.")
-  print("Type a:")
-  a = float(input())
-  print("Type b:")
-  b = float(input())
+  print("Choose the function you want to test! You can choose between A, B and C.")
+  op = input()
 
-  if f(a) * f(b) < 0:
-    print("Bisection method can be applied!")
-    bisection_method(a, b, 0, 0)
-    #print(a, b)
-  else:
-    print("Sorry! Bisection method can not be used in this interval!")
+  print("How many intervals you want to try?")
+  n = int(input())
 
+  for i in range(n):
+    print("Type the interval [a, b] to see if there is a way to apply bisection method.")
+    print("Type a:")
+    a = float(input())
+    print("Type b:")
+    b = float(input())
+
+    if f(a, op) * f(b, op) < 0:
+      print("Bisection method can be applied!")
+      bisection_method(a, b, 0, op, 0)
+    else:
+      print("Sorry! Bisection method can not be used in this interval!")
 
 if __name__ == "__main__":
     main()
