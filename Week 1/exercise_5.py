a7630e65836f2fdd412ce6d5ffe59838576e6526
Leonardo Almeida <leonardodealm@gmail.com>
2017-03-19 11:30:59 -0300

    Adding test cases with files

--- Week 1/exercise_5.py
+++ Week 1/exercise_5.py
@@ -28,22 +28,37 @@ def main():
   print("Choose the function you want to test! You can choose between A, B and C.")
   op = input()
 
-  print("How many intervals you want to try?")
-  n = int(input())
+  print("Type the name of test file [empty for skip]: ")
+  file_name = input()
+  try:
+    test_file = open(file_name, 'r')
+    for line in test_file:
+      a = float(line.strip().split()[0])
+      b = float(line.strip().split()[1])
 
-  for i in range(n):
-    print("Type the interval [a, b] to see if there is a way to apply bisection method.")
-    print("Type a:")
-    a = float(input())
-    print("Type b:")
-    b = float(input())
+      if f(a, op) * f(b, op) < 0:
+        print("Bisection method can be applied!")
+        bisection_method(a, b, 0, op, 0)
+      else:
+        print("Sorry! Bisection method can not be used in this interval!")
 
-    if f(a, op) * f(b, op) < 0:
-      print("Bisection method can be applied!")
-      bisection_method(a, b, 0, op, 0)
-    else:
-      print("Sorry! Bisection method can not be used in this interval!")
+    test_file.close()
+  except IOError:
+    print("How many intervals you want to try?")
+    n = int(input())
 
+    for i in range(n):
+      print("Type the interval [a, b] to see if there is a way to apply bisection method.")
+      print("Type a:")
+      a = float(input())
+      print("Type b:")
+      b = float(input())
+
+      if f(a, op) * f(b, op) < 0:
+        print("Bisection method can be applied!")
+        bisection_method(a, b, 0, op, 0)
+      else:
+        print("Sorry! Bisection method can not be used in this interval!")
 
 if __name__ == "__main__":
   main()
