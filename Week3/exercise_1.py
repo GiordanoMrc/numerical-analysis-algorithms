c503ebfb9a0c4c42aeba774c1fd43651b2bea6d0
Leonardo Almeida <leonardodealm@gmail.com>
2017-03-28 22:14:21 -0300

    Solving global variables

--- Week3/exercise_1.py
+++ Week3/exercise_1.py
@@ -4,23 +4,22 @@ Use the Secant Method to find the (single) solution of each equation in Exercise
 
 from math import *
 
-op
+global op
 
 def f(x):
-  print(op)
-  if op == 'A':
-    return (-1)*x**3 + 2*x + 2
-  if op == 'B':
-    return pow(e, x) + x - 7
-  return pow(e, x) + sin(x) - 4
+    if op == 'A':
+        return (-1)*x**3 + 2*x + 2
+    if op == 'B':
+        return pow(e, x) + x - 7
+    return pow(e, x) + sin(x) - 4
 
 def equal(xk1, xk):
-  m = abs(xk1 - xk)
-  e = 10**(-6) / 2.0
+    m = abs(xk1 - xk)
+    e = 10**(-6) / 2.0
 
-  if m < e:
-      return True
-  return False
+    if m < e:
+        return True
+    return False
 
 def secant_method(xk, xk_1):
     if(f(xk) == 0 or equal(xk, xk_1)):
@@ -31,9 +30,10 @@ def secant_method(xk, xk_1):
 
 
 def main():
-  print("Choose the function you want to test! You can choose between A, B and C.")
-  op = input()
-  print("Utilizando o método da secante encontramos a raiz ", secant_method(2, 1), " na função ", op);
+    global op
+    print("Choose the function you want to test! You can choose between A, B and C.")
+    op = input()
+    print("Utilizando o método da secante encontramos a raiz ", secant_method(2, 1), " na função ", op);
 
 if __name__ == "__main__":
     main()
